02d6852c855f5a7ffa92495294440b607177d50e
Md. Abdul Rakib <abdurrakib528@gmail.com>
2022-04-03 15:49:57 +0600

    Hero section design with slider

---
+++ trave-hero-section/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".owl-carousel").owlCarousel({
+          loop:true,
+          dots: true,
+          nav: true,
+          items: 1,
+           navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+        });    
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
