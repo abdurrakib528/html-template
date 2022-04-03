fbf23786fa7c7c45363c2ea8b0a1f13d62ad8306
Md. Abdul Rakib <abdurrakib528@gmail.com>
2022-04-03 15:20:12 +0600

    Hero section slider

---
+++ typeform-slider/assets/js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+        $(".type-form-text-slider").owlCarousel({
+            items:1,
+            nav:false,
+            dots:true,
+            loop: true,
+            autoplay:true,
+            smartSpeed:450,
+            slideSpeed: 250,
+            paginationSpeed: 200,
+        });
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
