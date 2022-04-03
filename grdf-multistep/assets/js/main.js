8362df8fa96710026f64f0cc2e0d8a10e8b9a439
Md. Abdul Rakib <abdurrakib528@gmail.com>
2022-04-03 15:25:17 +0600

    GRDF multistep page from PSD

---
+++ grdf-multistep/assets/js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+        $(".section-one-inner .block-list li span").on("click", function () {
+            $(this).toggleClass("active");
+        });
+
+    
+
+
+
+
+    });
+    
+})(jQuery);
